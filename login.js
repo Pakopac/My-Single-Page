9735d850f7f0540fc03075d32c18185092f0cbba
lilian pacaud <lilian.pacaud@supinternet.fr>
2018-02-06 22:24:51 +0100

    Verify login with Session Storage

--- login.js
+++ login.js
@@ -7,8 +7,21 @@ window.onload = function () {
 
         request.onload = function () {
             if (this.status >= 200 && this.status < 400) {
+
                 var resp = this.response;
-                document.querySelector('body').innerHTML = resp
+                document.querySelector('body').innerHTML = resp;
+
+                document.forms['loginForm'].onsubmit = function () {
+                    var pseudo = document.querySelector('#pseudo').value;
+                    var password = document.querySelector('#password').value;
+                    //sessionStorage.clear();
+                    //sessionStorage.setItem('pseudo', pseudo);
+                    //sessionStorage.setItem('password', password);
+
+                    if (pseudo === sessionStorage.getItem("pseudo") && password === sessionStorage.getItem("password")){
+                        console.log('ok')
+                    }
+                }
             } else {
 
             }
