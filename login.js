07cbfe1789468d3172622732a7489b52dc98fc5e
Pakopac <lilian.pacaud@supinternet>
2018-02-09 16:14:39 +0100

    Redirect to home when user log in

--- login.js
+++ login.js
@@ -1,7 +1,6 @@
 window.onload = function () {
-    var linkLogin = document.querySelector('#linkLogin');
+    document.querySelector('#menu').style.display = 'none';
 
-    linkLogin.onclick = function () {
         var request = new XMLHttpRequest();
         request.open('GET', 'templates/login.html', true);
 
@@ -9,7 +8,7 @@ window.onload = function () {
             if (this.status >= 200 && this.status < 400) {
 
                 var resp = this.response;
-                document.querySelector('body').innerHTML = resp;
+                document.querySelector('#login').innerHTML = resp;
 
                 document.forms['loginForm'].onsubmit = function () {
                     var pseudo = document.querySelector('#pseudo').value;
@@ -19,8 +18,11 @@ window.onload = function () {
                     //sessionStorage.setItem('password', password);
 
                     if (pseudo === sessionStorage.getItem("pseudo") && password === sessionStorage.getItem("password")){
-                        console.log('ok')
+                        console.log(document.querySelector('body').innerHTML);
+                        var login = document.querySelector('#login');
+                        login.replaceWith(document.querySelector('#menu').innerHTML);
                     }
+                    return false
                 }
             } else {
 
@@ -32,6 +34,5 @@ window.onload = function () {
         };
 
         request.send();
-        return false
-    };
+
 };
